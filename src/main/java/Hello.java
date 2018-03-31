4b3468a16657fa7692ff87985365116dbd4d7ae6
SangHyeukYoon <nyoonsh97@gmail.com>
2018-03-31 13:45:37 +0900

    Add Test

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,4 +2,10 @@ public class Hello {
     public static void main(String args[]) {
         System.out.println("Hello Gradle!");
     }
+
+    public static int Add() {
+        int result = 10 + 20;
+
+        return result;
+    }
 }
